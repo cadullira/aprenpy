7a067cbf5af5343c6217f62c0c430d7dafe0033e
Cadu Lira <cadullira@gmail.com>
2024-01-03 04:48:06 -0300

    Inaugura os commits para o estudo de python

---
+++ exercicios/ex11.py
@@ -0,0 +1,12 @@
+# Exercícios encontrados no capítulo 1 do Pense em Python (2ª ed.) 
+# https://penseallen.github.io/PensePython2e/01-jornada.html
+
+# print('Olá'
+# print "Olá"
+# print('Oi)
+# print(Oi)
+# print(+2)
+# print(type(+2))
+# print(2++2)
+# print (02)
+# print (2 2)
