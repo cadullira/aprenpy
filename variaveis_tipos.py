a1d48f3f8df8f7be82edb25dcb6843580dc8957d
Cadu Lira <cadullira@gmail.com>
2024-01-04 04:13:03 -0300

    Explana variáveis e seus tipos
    - ver o funcionamento das funções type e isinstance

---
+++ variaveis_tipos.py
@@ -0,0 +1,21 @@
+#nome = 'Cadu' # tipo string
+#print(nome) # nome é o argumento da função print
+
+media = 10 # tipo int
+n1 = n2 = 0.0 # tipo float
+nome, idade = 'Duda', 31
+estado = True
+
+# type(parametro) retorna de qual tipo de dado é determinada variável
+# print(type(media))
+# print(type(n1))
+# print(type(nome))
+# print(type(estado))
+
+# isInstance(parametro) retorna um boolean, primeiro parâmetro a variável, a segunda o tipo, daí vai ser perguntado se variável é daquele tipo
+
+a = 8
+b = 'lua'
+
+print(isinstance(8, int))
+print(isinstance(a, str))
