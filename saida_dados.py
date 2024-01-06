4bd5c5a6d60491fe0d8918b371f61f6658d39476
Cadu Lira <cadullira@gmail.com>
2024-01-06 04:36:21 -0300

    Apresenta o operador para exibição na tela
    - posicional (,)
    - concatenação (+)
    - .format()
    - fString

---
+++ saida_dados.py
@@ -0,0 +1,27 @@
+# Sintaxe
+#print(objetos, argumentos)
+
+# mensagem = 'Função print()'
+# print(mensagem)
+# print('Python')
+
+# #posicional -> separação por vírgula
+# escola = 'Turma da Mônica'
+# print('Ele estuda na escola', escola)
+
+# #concatenação -> usa o símbolo de +  
+# nome = input('Digite o seu nome: ')
+# print('Bem vindo ao curso '+ nome)
+
+#.format
+# time = 'Vasco'
+# colocacao = 15
+
+# print('O {0} terminou o campeonato na {1}ª colocação'.format(time, colocacao))
+
+#f(String)
+padre = 'Claúdio'
+valor = 10.234965
+
+#print(f'O pároco da nossa cidade se chama {padre}')
+print(f'valor arredondado {valor:.2f}')
